f6ed680224733dbc88b791e9fbb8044f1e00a981
Noah <noah@noah.kr>
2018-10-14 18:42:16 +0900

    section 11 complete

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View />
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
